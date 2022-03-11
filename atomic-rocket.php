59bf7ac1219c9872d9636f3e0db1cf8d58d35606
Nicolas Harild Andersen <nicolasharild@outlook.dk>
2022-03-11 08:36:47 +0000

    info side

--- atomic-rocket.php
+++ atomic-rocket.php
@@ -9,14 +9,15 @@
 <!-- Navigations bar -->
 <nav>
 <div class="topnav">
-<div class="logo"><a>DINOGAMING</a></div>
+<div class="logo"><a href="index.php">DINOGAMING</a></div>
 <div class="item"><img src="media/basket.svg"></div>
 </div>
 </nav>
 
 <div class="main-container">
 <div class="main-container-row">
-
+<div class="sub-container-info">
+</div>
 </div>
 </div>
 </body>
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <!-- Navigations bar -->
 <nav>
 <div class="topnav">
-<div class="logo"><a>DINOGAMING</a></div>
+<div class="logo"><a href="index.php">DINOGAMING</a></div>
 <div class="item"><img src="media/basket.svg"></div>
 </div>
 </nav>
