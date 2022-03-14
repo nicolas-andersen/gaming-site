ffd0218a61442e62e09c240604383c1b9c7ebda8
nicolas-andersen <nicolasharild@outlook.dk>
2022-03-14 10:53:49 +0100

    rocket jumper info side

--- index.php
+++ index.php
@@ -15,23 +15,23 @@
 </nav>
 <div class="main-container">
 <div class="main-container-row">
-<div style="background-size: 600px; background-image: url('media/computer1.png');" class="sub-container">
+<div style="background-size: 600px; background-image: url('media/atomic-rocket.png');" class="sub-container">
 	<div class="sub-container-text">
 	<h1>ATOMIC ROCKET</h1><br>
 	<p>Nok måske den stærkeste gaming computer på denne planet.</p>
 	<a href="atomic-rocket.php" class="button">Køb her</a>
 	</div>
 </div>
-<div style="background-size: 1000px; background-image: url('media/computer2.jpg');" class="sub-container">
+<div style="background-size: 1000px; background-image: url('media/rocket-jumper.jpg');" class="sub-container">
 	<div class="sub-container-text">
 	<h1>ROCKET JUMPER</h1><br>
 	<p>Den mest populære computer blandt Team Fortress 2 spillere.</p>
-	<a href="https://sharkgaming.dk" class="button">Køb her</a>
+	<a href="rocket-jumper.php" class="button">Køb her</a>
 	</div>
 </div>
 </div>
 <div class="main-container-row">
-	<div style="background-size: 1300px; background-image: url('media/computer4.jpg');" class="sub-container">
+	<div style="background-size: 1300px; background-image: url('media/atomic-watermelon.jpg');" class="sub-container">
 	<div class="sub-container-text">
 	<h1>ATOMIC WATERMELON</h1><br>
 	<p>En gaming bærebar uden kompromis!</p>
