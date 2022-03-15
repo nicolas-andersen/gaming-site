d4f2ab93bb9a07091b83a6cdfa09b32ca53f39c9
nicolas-andersen <nicolasharild@outlook.dk>
2022-03-15 11:20:01 +0100

    wwg

--- basket.php
+++ basket.php
@@ -53,7 +53,7 @@ if ($products_size == 1) {
 <?php
 for ($i = 1; $i <= $items_size; $i++) {
 	if (isset($_COOKIE[strval($i)])) {
-		$image = mysqli_fetch_field(mysqli_query($connection, "SELECT image FROM products where id IN(" . $i . ")"));
+		$image = mysqli_fetch_array(mysqli_query($connection, "SELECT image FROM products where id IN(" . $i . ")"))[0];
 
 		echo "<div class=\"basket-item\">". 
 			 "<img src=\"" . $image . "\" class=\"basket-item-image\">" .
